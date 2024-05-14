cd616ff8e8105f30b90a1f570e042a832d3cfa83
Oliveira_Yan <85714204+Yan-Oliv@users.noreply.github.com>
2024-05-14 15:00:15 +0000

    Resolução do exercício

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,19 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Yan'
+profissao = 'Analista T.I'
+objetivo = ''' Aprender python
+para 
+mudar
+de
+emprego
+e 
+área'''
 # Declare variáveis de cada tipo numérico
-
+idade = 21
+salario_atual = 1700,00
 
 # Declare uma variável do tipo booleano
+boolean_idade = True
+boolean_name = True
